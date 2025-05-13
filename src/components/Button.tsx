02da57e979d15d3a23f2c2c5144d484d5459e450
LBeukennoot <lars@beukenkamp.net>
2025-05-14 01:34:21 +0200

    changes visual style of Button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,7 @@
 export default function Button({ text, onClick }: ButtonType) {
 
     return (
-        <button className="bg-blue text-lg text-white px-6 py-2.5 rounded-lg cursor-pointer" onClick={(e) => {onClick(e)}}>
+        <button className="bg-blue text-xl text-white px-7 py-2 rounded-full border-6 border-blue hover:bg-light-blue cursor-pointer transition-all duration-150" onClick={(e) => {onClick(e)}}>
             {text}
         </button>
     )
