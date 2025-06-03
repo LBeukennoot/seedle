8c25395d940349c73ceec5a204a332f0a90e3d41
LBeukennoot <lars@beukenkamp.net>
2025-06-03 12:51:43 +0200

    Changes style and function of dropdown

--- src/components/dropdown/ArrowDownIcon.tsx
+++ src/components/dropdown/ArrowDownIcon.tsx
@@ -1,8 +1,8 @@
 import { IconType } from "../navigation/icons/IconType";
 
-export default function ChangelogIcon({ className }: IconType) {
+export default function ChangelogIcon({ className, ...props }: IconType) {
     return (
-        <svg className={className} width="16" height="9" viewBox="0 0 16 9" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <svg {...props} className={className} width="16" height="9" viewBox="0 0 16 9" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M15 1L8 8L1 1" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
         </svg>
     )
--- src/components/navigation/icons/IconType.tsx
+++ src/components/navigation/icons/IconType.tsx
@@ -1,3 +1,4 @@
 export type IconType = {
     className: string
+    id?: string
 }
