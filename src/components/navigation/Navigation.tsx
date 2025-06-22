1a411c3560c0380d1be25d6c05897d6f9807fca3
LBeukennoot <lars@beukenkamp.net>
2025-06-22 17:00:56 +0200

    changes screen corners to rounded

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -2,36 +2,27 @@ import { useContext, useState } from "react"
 import { NavigationContext } from "../../providers/NavigationProvider"
 import Tablist from "../Tablist"
 import ScreenCard from "./screens/ScreenCard"
-import { useDraggable } from "@dnd-kit/core"
 
 export default function Navigation() {
 
-    const { ScreenElement, expanded } = useContext(NavigationContext)
-
-    const [height, setHeight] = useState(0);
+    const { ScreenElement } = useContext(NavigationContext)
 
     return (
         <div className="w-screen h-screen flex bg-light-green text-blue font-lexend items-center justify-center">
-
             <div className="flex justify-center md:items-center h-full w-full md:w-auto m-2 md:m-0">
+                <div className="md:flex items-start h-[30rem] w-full md:w-auto mt-2 md:m-0 relative">
 
-                <div className="md:flex items-start h-[30rem] w-full md:w-auto mt-2 md:m-0">
-                    <div className={"z-10 " + (expanded ? "" : "m-auto md:-ml-15 md:absolute max-w-xl")}>
-                        <Tablist expanded={expanded} />
+                    <div className="z-20 relative mt-15 md:mt-0">
+                        <ScreenCard>
+                            <ScreenElement />
+                        </ScreenCard>
                     </div>
 
+                    <div className={"z-10 top-0 m-auto md:-ml-15 absolute "}>
+                        <Tablist />
+                    </div>
 
-                    <ScreenCard>
-                        <ScreenElement />
-
-                        {/* TODO drag down functionality
-                        <div className="relative flex md:hidden justify-center">
-                            <div className="absolute float bg-blue h-1.5 w-20 rounded-full mt-1">
-                            </div>
-                        </div> */}
-                    </ScreenCard>
                 </div>
-
             </div>
         </div>
     )
--- src/components/navigation/screens/ScreenCard.tsx
+++ src/components/navigation/screens/ScreenCard.tsx
@@ -2,10 +2,9 @@ import { useContext } from "react"
 import { NavigationContext } from "../../../providers/NavigationProvider"
 
 export default function ScreenCard({ children, ...props }: any) {
-    const { expanded, setExpanded } = useContext(NavigationContext)
 
     return (
-        <div {...props} className={"bg-white mx-auto max-w-xl rounded-tl-none rounded-[3.5rem] max-h-[30rem] z-20 transition-all " + (expanded ? "" : "p-5 w-full md:p-10 md:min-h-[15rem] md:min-w-[30rem]")}>
+        <div {...props} className={"bg-white z-20 mx-auto max-w-xl rounded-[3.5rem] max-h-[30rem] transition-all p-5 w-full md:p-10 md:min-h-[15rem] md:min-w-[30rem]"}>
             <div className="max-h-[25rem]">
                 {children}
             </div>
