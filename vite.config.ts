d1ccb00821a7d59ea75375c2fcad627e75a7cd80
LBeukennoot <lars@beukenkamp.net>
2026-04-22 16:18:17 +0200

    updates entry file

--- vite.config.ts
+++ vite.config.ts
@@ -43,7 +43,7 @@ export default defineConfig({
   },
   build: {
     lib: {
-      entry: './src/index.ts', // Entry point for the library
+      entry: './src/main.tsx', // Entry point for the library
       name: 'ui', // Global variable name
       fileName: (format) => `ui.${format}.js`, // Output filenames
       formats: ['es', 'cjs'], // Output both ESModule and CommonJS builds
